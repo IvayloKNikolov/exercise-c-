9f2e1c2d2c1c0572c8006557dfe5033746836077
IvayloKNikolov <32244510+IvayloKNikolov@users.noreply.github.com>
2017-09-28 16:49:29 +0300

    Add files via upload

---
+++ 14 Magic Letter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14.Magic_Letter
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            char first = char.Parse(Console.ReadLine());
+            char second = char.Parse(Console.ReadLine());
+            char invalid = char.Parse(Console.ReadLine());
+            
+
+            for (char i = first; i <= second; i++)
+            {
+                for (char j = first; j <= second; j++)
+                {
+                    for (char k = first; k <= second; k++)
+                    {
+                        string final = "" +i+j+k;
+                        if (!final.Contains(invalid + ""))
+                        {
+                            Console.Write(final + " ");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.Word_in_Plural
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string pluralNoun = Console.ReadLine();
+            if (pluralNoun.EndsWith("y"))
+            {
+                pluralNoun = pluralNoun.Remove(pluralNoun.Length - 1);
+                pluralNoun = pluralNoun + "ies";
+            }
+
+            else if (pluralNoun.EndsWith("o") || pluralNoun.EndsWith("x") || pluralNoun.EndsWith("s") || pluralNoun.EndsWith("z") || pluralNoun.EndsWith("ch") || pluralNoun.EndsWith("sh"))
+            {
+                pluralNoun = pluralNoun + "es";
+            }
+            else
+            {
+                pluralNoun = pluralNoun + "s";
+            }
+
+
+            Console.WriteLine(pluralNoun);
+
+
+
+
+        }
+}
+}
