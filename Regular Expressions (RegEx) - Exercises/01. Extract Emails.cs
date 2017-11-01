8094fbc00282cb049bb195f9d781907faed7cb21
IvayloKNikolov <32244510+IvayloKNikolov@users.noreply.github.com>
2017-11-01 12:34:05 +0200

    Add files via upload

---
+++ Regular Expressions (RegEx) - Exercises/01. Extract Emails.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace _01.Extract_Emails
+{
+    class Program
+    {
+        static void Main()
+        {
+            string toRegex = @"(?<=\s)[a-z0-9]+([-.]\w*)*@[a-z]{1,}([-.]\w*)*(\.[a-z]{1,})";
+
+            var regexemails = new Regex(toRegex);
+            string textToMatch = Console.ReadLine();
+
+            var content = regexemails.Matches(textToMatch);
+
+            foreach (Match mail in content)
+            {
+                Console.WriteLine(mail);
+            }
+        }
+    }
+}
---
+++ Regular Expressions (RegEx) - Exercises/05. Key Replacer.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace _05.Key_Replacer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex split = new Regex(@"([a-zA-Z]*)(?:\<|\||\\)(?:.+)(?:\<|\||\\)(\w+)");
+
+            string firstInput = Console.ReadLine();
+            string secondInput = Console.ReadLine();
+
+            string start = "";
+            string end = "";
+
+            var matcher = split.Matches(firstInput);
+            foreach (Match match in matcher)
+            {
+                start = match.Groups[1].Value.ToString();
+                end = match.Groups[2].Value.ToString();
+            }
+
+            var solution = new StringBuilder();
+
+            int indexStart = 0;
+            int indexEnd = 0;
+
+            for (int i = 0; i < secondInput.Length; i++)
+            {
+                int firstOccasion = secondInput.IndexOf(start, indexStart);
+                if (firstOccasion == -1)
+                {
+                    break;
+                }
+                int secondOccasion = secondInput.IndexOf(end, indexEnd);
+
+                if (firstOccasion != -1 && secondOccasion != -1)
+                {
+                    solution.Append(secondInput, firstOccasion + start.Length, secondOccasion - firstOccasion - start.Length);
+
+                    indexStart = secondOccasion + end.Length;
+                    indexEnd = indexStart + 1;
+                }
+            }
+            if (solution.Length > 0)
+            {
+                Console.WriteLine(solution);
+            }
+            else
+            {
+                Console.WriteLine("Empty result");
+            }
+        }
+    }
+}
